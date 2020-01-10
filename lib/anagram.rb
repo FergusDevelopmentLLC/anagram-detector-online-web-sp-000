39d7dbcb27d7700a4bf788e9f4e03566fdcbf9fb
Will Carter <will.carter@fergusllc.com>
2020-01-10 22:15:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,15 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
+
   def initialize(word)
     @word = word
   end
+  
+  def match(words)
+  end
+  
 end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
